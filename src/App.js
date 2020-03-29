c40027c925428fb6ca286285b7e9a9d847b74c71
RichardKim <hiddeneventor@gmail.com>
2020-03-30 00:38:50 +0900

    map 사용

--- src/App.js
+++ src/App.js
@@ -2,26 +2,55 @@ import React from 'react';
 import Customer from './components/Customer';
 import './App.css';
 
-const customer = {
+const customers = [
+  {
   'id': 1,
-  'image': 'https://placeimg.com/64/64/any',
+  'image': 'https://placeimg.com/64/64/1',
   'name' : '한글이름',
   'birthday' : '20200109',
   'gender': 'men',
   'job': 'developer',
+},
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name' : '2배열 이름',
+  'birthday' : '20200109',
+  'gender': 'men',
+  'job': 'developer',
+},
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name' : '3배열 이름',
+  'birthday' : '20200109',
+  'gender': 'men',
+  'job': '그냥 직업',
 }
+]
 
 
 function App() {
   return (
-    <Customer
-      id = {customer.id}
-      image = {customer.image}
-      name = {customer.name}
-      birthday = {customer.birthday}
-      gender = {customer.gender}
-      job = {customer.job}
-    />
+    <div>
+      {
+
+        customers.map(c => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender = {c.gender}
+              job= {c.job}
+            />
+          );
+        })
+
+      }
+    </div>
   );
 }
 
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 
 class Customer extends React.Component{
     render() {
