195afce63b6dba86dc230da0e07b985f31348073
RichardKim <hiddeneventor@gmail.com>
2020-03-29 23:54:09 +0900

    react components Test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer';
 import './App.css';
+
+const customer = {
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/any',
+  'name' : '한글이름',
+  'birthday' : '20200109',
+  'gender': 'men',
+  'job': 'developer',
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Customer
+      id = {customer.id}
+      image = {customer.image}
+      name = {customer.name}
+      birthday = {customer.birthday}
+      gender = {customer.gender}
+      job = {customer.job}
+    />
   );
 }
 
